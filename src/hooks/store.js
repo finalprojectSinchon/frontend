15bf1c55d291fa772fc02d11c20070fac52373eb
yoonjae <realyoon77@gmail.com>
2024-07-14 01:26:06 +0900

    [2024-07-14] 로그인

---
+++ src/hooks/store.js
@@ -0,0 +1,16 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+
+let user = createSlice({
+    name : 'user',
+    initialState : {
+        
+    }
+})
+
+
+export default configureStore({
+    reducer : { 
+
+    }
+})
