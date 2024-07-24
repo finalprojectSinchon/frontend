4daea3a9162b4141f0b0b890cefa763b506a4b4b
jieun <ggeun1031@naver.com>
2024-07-24 17:17:31 +0900

    [2024-07-24] 체크인 카운터/ 수화물 수취대 등록

--- src/views/airplane/CheckinCounterRegist.js
+++ src/views/airplane/CheckinCounterRegist.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Card,
   CardBody,
@@ -12,40 +12,82 @@ import {
   Button,
 } from 'reactstrap';
 import BreadCrumbs from '../../layouts/breadcrumbs/BreadCrumbs';
-import { useNavigate ,useLocation } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-
-
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAirplanes } from '../../store/apps/airplane/airplaneSlice';
 
 const CheckinCounterDetail = () => {
   const location = useLocation();
-  const state = location.state || {}; 
-
-  console.log('location',location)
-  console.log('state',state)
-
+  const state = location.state || {};
   const dispatch = useDispatch();
-  const navigate = useNavigate();
+  
+  const AirplaneList = useSelector((state) => state.airplanes.airplaneList);
+  const airplanes = AirplaneList?.data?.airplaneList || [];
 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const [checkinCounterInfo, setCheckinCounterInfo] = useState({});
- 
+  const [selectedAirline, setSelectedAirline] = useState('');
+  const [selectedSchedule, setSelectedSchedule] = useState('');
+  const [arrivalTimes, setArrivalTimes] = useState([]);
+  const [flightId, setFlightId] = useState('');
+  const [airport, setAirport] = useState('');
 
-  const onChangeHandler = e => {
-    setCheckinCounterInfo({
-      ...checkinCounterInfo,
-      [e.target.name]: e.target.value
-    });
+  useEffect(() => {
+    dispatch(fetchAirplanes())
+      .then(() => setLoading(false))
+      .catch((err) => {
+        setLoading(false);
+        setError(err.message || '데이터 로딩 실패');
+      });
+  }, [dispatch]);
+
+  const ChangeHandler = (event) => {
+    const { name, value } = event.target;
+    console.log('name', name);
+
+    if (name === 'airline') {
+      setSelectedAirline(value);
+
+      const filteredTimes = airplanes
+        .filter(airplane => airplane.airline === value)
+        .map(airplane => airplane.scheduleDateTime);
+      setArrivalTimes(filteredTimes);
+      setSelectedSchedule(''); 
+      console.log('filteredTimes', filteredTimes);
+
+    } else if (name === 'scheduleDateTime') {
+      setSelectedSchedule(value);
+    }
   };
 
-  
- 
+  const selectedAirplane = airplanes.find(airplane =>
+    airplane.airline === selectedAirline &&
+    airplane.scheduleDateTime === selectedSchedule
+  );
 
-  const handleClick = () => {
-   
   
- 
+
+  useEffect(() => {
+    if (selectedAirplane) {
+      setFlightId(selectedAirplane.flightId || '');
+      setAirport(selectedAirplane.airport || '');
+    } else {
+      setFlightId('');
+      setAirport('');
     }
+  }, [selectedAirplane]);
+
+  const uniqueAirlines = [...new Set(airplanes.map(airplane => airplane.airline))];
+
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div>
@@ -61,30 +103,68 @@ const CheckinCounterDetail = () => {
             <CardBody>
               <Form>
                 <Row>
-                <Col md="6">
+                  <Col md="6">
                     <FormGroup>
                       <Label>위치</Label>
-                      <Input type="text" name="location"  onChange={onChangeHandler}  />
+                      <Input type="text" name="location" value={state.location || ''} />
+                    </FormGroup>
+                  </Col>
+                  <Col md="6">
+                    <FormGroup>
+                      <Label>항공사</Label>
+                      <Input type="select" name='airline' onChange={ChangeHandler}>
+                        <option value="">항공사를 선택하세요</option>
+                        {uniqueAirlines.map((airline, index) => (
+                          <option key={index} value={airline}>
+                            {airline}
+                          </option>
+                        ))}
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md="6">
+                    <FormGroup>
+                      <Label>도착예정시간</Label>
+                      <Input type="select" name='scheduleDateTime' value={selectedSchedule} onChange={ChangeHandler}>
+                        <option value="">도착 예정 시간을 선택하세요</option>
+                        {arrivalTimes.map((time, index) => (
+                          <option key={index} value={time}>{time}</option>
+                        ))}
+                      </Input>
                     </FormGroup>
                   </Col>
                   <Col md="6">
                     <FormGroup>
                       <Label>Status</Label>
-                      <Input type="select" name="status"  onChange={onChangeHandler} >
-                        <option>고장</option>
+                      <Input type="select" name="status">
                         <option>정상</option>
+                        <option>고장</option>
                         <option>점검중</option>
                       </Input>
                     </FormGroup>
                   </Col>
-                
                 </Row>
                 <Row>
-                    
+                  <Col md="6">
+                    <FormGroup>
+                      <Label>편명</Label>
+                      <Input type="text" name='flightId' value={flightId} readOnly />
+                    </FormGroup>
+                  </Col>
+                  <Col md="6">
+                    <FormGroup>
+                      <Label>최근 점검일</Label>
+                      <Input type="date" name="lastInspectionDate" />
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row>
                   <Col md="6">
                     <FormGroup>
                       <Label>type</Label>
-                      <Input type="select" name="gateType"  onChange={onChangeHandler} >
+                      <Input type="select" name="gateType">
                         <option>A</option>
                         <option>B</option>
                         <option>C</option>
@@ -94,36 +174,8 @@ const CheckinCounterDetail = () => {
                   </Col>
                   <Col md="6">
                     <FormGroup>
-                      <Label>항공사</Label>
-                      <Input type="text" name='airline' onChange={onChangeHandler}  />
-                    </FormGroup>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md="6">
-                    <FormGroup>
-                      <Label>편명</Label>
-                      <Input type="text" name='flightId' onChange={onChangeHandler}  />
-                    </FormGroup>
-                  </Col>
-                  <Col md="6">
-                    <FormGroup>
-                      <Label>최근 점검일</Label>
-                      <Input type="date"  name="lastInspectionDate" onChange={onChangeHandler}  />
-                    </FormGroup>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md="6">
-                    <FormGroup>
-                      <Label>도착예정시간</Label>
-                      <Input type="datetime" name='scheduleDateTime' onChange={onChangeHandler}/>
-                    </FormGroup>
-                  </Col>
-                  <Col md="6">
-                    <FormGroup>
                       <Label>지연시간</Label>
-                      <Input type="number"  name='delayTime' onChange={onChangeHandler} />
+                      <Input type="number" name='delayTime' />
                     </FormGroup>
                   </Col>
                 </Row>
@@ -131,30 +183,28 @@ const CheckinCounterDetail = () => {
                   <Col md="6">
                     <FormGroup>
                       <Label>도착공항명</Label>
-                      <Input type="text" name='airport' onChange={onChangeHandler}  />
+                      <Input type="text" name='airport' value={airport} readOnly />
                     </FormGroup>
                   </Col>
                   <Col md="6">
                     <FormGroup>
                       <Label>담당자</Label>
-                      <Input type="text" name="manager" onChange={onChangeHandler}  />
+                      <Input type="text" name="manager" />
                     </FormGroup>
                   </Col>
                 </Row>
                 <Row>
-                 
                   <Col md="6">
                     <FormGroup>
                       <Label>비고</Label>
-                      <Input type="textarea" rows="6"  name="note" onChange={onChangeHandler} />
+                      <Input type="textarea" rows="6" name="note" />
                     </FormGroup>
                   </Col>
                 </Row>
                 <Col className='d-flex justify-content-center'>
-                  <Button className="m-2" color="primary" onClick={handleClick}>
+                  <Button className="m-2" color="primary" onClick={() => handleClick()}>
                     등록
                   </Button>
-                
                 </Col>
               </Form>
             </CardBody>
@@ -164,12 +214,5 @@ const CheckinCounterDetail = () => {
     </div>
   );
 };
-
-
- 
-
-
-
-  
 
 export default CheckinCounterDetail;
