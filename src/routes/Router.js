5f614fc3c3ba2c5dd1c447073d5381eefc4cc7cf
jaejae <santoki389@gmail.com>
2024-07-24 16:17:21 +0900

    storage 상세조회

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -2,11 +2,6 @@ import { lazy } from 'react';
 import { Navigate } from 'react-router-dom';
 import Loadable from '../layouts/loader/Loadable';
 import ProfileUploader from '../views/auth/imgUpload';
-import { element, exact } from 'prop-types';
-//import MaintenanceRegist from '../views/maintenance/maintenanceRegist';
-
-
-
 
 
 /****Layouts*****/
@@ -20,21 +15,12 @@ const Airplane = Loadable(lazy(() => import('../views/airplane/Airplane')));
 const AirplaneDetail = Loadable(lazy(() => import('../views/airplane/AirplaneDetail')));
 const CheckinCounter = Loadable(lazy(() => import('../views/airplane/CheckinCounter')));
 const CheckinCounterDetail = Loadable(lazy(() => import('../views/airplane/CheckinCounterDetail')));
-const CheckinCounterRegist = Loadable(lazy(()=> import('../views/airplane/CheckinCounterRegist')));
 const Gate = Loadable(lazy(() => import('../views/airplane/Gate')));
 const GateDetail = Loadable(lazy(() => import('../views/airplane/GateDetail')));
 const BaggageClaim = Loadable(lazy(() => import('../views/airplane/BaggageClaim')));
 const BaggageClaimDetail = Loadable(lazy(() => import('../views/airplane/BaggageClaimsDetail')));
-const BaggageClaimRegist = Loadable(lazy(() => import('../views/airplane/BaggageClaimRegist')));
-// 편의시설
-const Facilities = Loadable(lazy(() => import('../views/facilities/facilities')));
-const FacilitiesDetail = Loadable(lazy(() => import('../views/facilities/facilitiesDetail')));
-
-
-
 
 const Profile = Loadable(lazy(() => import('../views/auth/Profile')));
-const AuthCode = Loadable(lazy(() => import('../views/auth/AuthCode.js')));
 
 const Inspection = Loadable(lazy(() => import('../views/inspection/inspection')));
 const InspectionDetail = Loadable(lazy(() => import('../views/inspection/inspectionDetail')));
@@ -45,13 +31,12 @@ const InspectionUpdate = Loadable(lazy(() => import('../views/inspection/inspect
 const AirportStore = Loadable(lazy(() => import('../views/airportStore/airportStore')));
 
 const Maintenance =  Loadable(lazy(() => import('../views/maintenance/maintenance')));
-const MaintenanceDetail = Loadable(lazy(() => import('../views/maintenance/maintenanceDetails')));
-const MaintenanceRegist = Loadable(lazy(() => import('../views/maintenance/maintenanceRegist')));
 
 const AirportStoreDetail = Loadable(lazy(() => import('../views/airportStore/airportStoreDetail')));
 const AirportDBUpdate = Loadable(lazy(() => import('../views/airportStore/airportStoreDBUpdate')));
 
 const Storage = Loadable(lazy(() => import('../views/storage/Storage')));
+const StorageDetail = Loadable(lazy(() => import('../views/storage/StorageDetail')));
 
 
 
@@ -62,8 +47,6 @@ const CASL = Loadable(lazy(() => import('../views/apps/accessControlCASL/AccessC
 const Error = Loadable(lazy(() => import('../views/auth/Error')));
 const RegisterFormik = Loadable(lazy(() => import('../views/auth/RegisterFormik')));
 const LoginFormik = Loadable(lazy(() => import('../views/auth/LoginFormik')));
-const PermissionError = Loadable(lazy(() => import('../views/auth/PermissionError.js')));
-const Certification = Loadable(lazy(() => import('../views/auth/Certification.js')));
 
 
 /*****Routes******/
@@ -78,20 +61,17 @@ const ThemeRoutes = [
       { path: '/airplane/:airplaneCode', name: '비행기 상세보기', exact: true, element: <AirplaneDetail/> },
       { path: '/airplane/checkin-counter', name: 'CheckinCounter', exact: true, element: <CheckinCounter /> },
       { path: '/airplane/checkin-counter/:checkinCounterCode', name: 'CheckinCounterDetail', exact: true, element: <CheckinCounterDetail /> },
-      { path: '/airplane/checkin-counter/regist', name: 'CheckinCounterRegist', exact: true, element: <CheckinCounterRegist /> },
       { path: '/airplane/gate', name: 'Gate', exact: true, element: <Gate /> },
       { path: '/airplane/gate/:gateCode', name: 'GateDetail', exact: true, element: <GateDetail /> },
       { path: '/airplane/baggage-claim', name: 'BaggageClaim', exact: true, element: <BaggageClaim /> },
       { path: '/airplane/baggage-claim/:baggageClaimCode', name: 'BaggageClaimDetail', exact: true, element: <BaggageClaimDetail /> },
-      { path: '/airplane/baggage-claim/regist', name: 'BaggageClaimRegist', exact: true, element: <BaggageClaimRegist /> },
 
       { path: '/api/v1/maintenance', name: 'Maintenance', exact: true, element: <Maintenance /> },
       { path: '/api/v1/inspection', name: 'inspection', exact: true, element: <Inspection/> },
       { path: '/api/v1/inspection/:inspectionCode', name: 'inspectionDetail', exact: true, element: <InspectionDetail/> },
       { path: '/api/v1/inspection/inspectionUpdate', name: 'inspectionUpdate', exact: true, element: <InspectionUpdate/> },
 
-      { path: '/facilities', name: 'Facilities', exact: true, element: <Facilities /> },
-      { path: '/facilities/:facilitiesCode', name: 'FacilitiesDetail', exact: true, element:<FacilitiesDetail /> },
+
 
       { path: '/dashboards/dashboard1', name: 'Dashboard 1', exact: true, element: <Dashboard1 /> },
   
@@ -99,13 +79,12 @@ const ThemeRoutes = [
       { path: '/airport/store/:storeId', name: 'AirportStoreDetail', exact: true, element: <AirportStoreDetail /> },
       { path: '/airport/store/dbupdate', name: 'AirportStoreDBUpdate', exact: true, element: <AirportDBUpdate /> },
       { path: '/storage', name: 'Storage', exact: true, element: <Storage /> },
+      { path: '/storage/:storageCode', name: 'StorageDetail', exact: true, element: <StorageDetail /> },
 
 
 
       { path: '/profile', name: 'profile', exact: true, element: <Profile /> },
       { path: '/upload', name: 'test', exact: true, element: <ProfileUploader /> },
-      { path: '/code-issuance', name: 'test', exact: true, element: <AuthCode /> },
-
 
     
       { path: '/casl', name: 'casl', exact: true, element: <CASL /> },
@@ -120,8 +99,6 @@ const ThemeRoutes = [
       { path: '*', element: <Navigate to="/auth/404" /> },
       { path: 'registerformik', element: <RegisterFormik /> },
       { path: 'loginformik', element: <LoginFormik /> },
-      { path: 'permission-error', element: <PermissionError /> },
-      { path: 'certification', element: <Certification /> },
     ],
   },
 ];
