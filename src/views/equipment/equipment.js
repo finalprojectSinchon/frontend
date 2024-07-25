b0f3a1616fd8e47b11e8b7c831fe059f44cec631
DESKTOP-0CUIJL6\LEE <h2odream@naver.com>
2024-07-25 17:44:17 +0900

    [2024.07.25]equipment_quantity 전체 조회

---
+++ src/views/equipment/equipment.js
@@ -0,0 +1,169 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { Card, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchEquipments } from '../../store/apps/equipment/equipmentSlice';
+
+function onAfterDeleteRow(rowKeys) {
+    alert(`The rowkey you drop: ${rowKeys}`);
+}
+
+function afterSearch(searchText, result) {
+    console.log(`Your search text is ${searchText}`);
+    console.log('result', result);
+}
+
+const selectRowProp = {
+    mode: 'checkbox', // 체크박스 모드 유지
+    clickToSelect: true,
+    bgColor: 'rgba(0,0,0,0.1)',
+    clickToExpand: true,
+    onSelect: (row, isSelected, rowIndex, e) => {
+        console.log(`Row selected: ${isSelected}, rowIndex: ${rowIndex}`);
+        console.log('Selected row: ', row);
+    },
+    onSelectAll: (isSelected, rows) => {
+        console.log(`All rows selected: ${isSelected}`);
+        console.log('Selected rows: ', rows);
+    }
+};
+
+
+
+const cellEditProp = {
+    mode: 'click',
+    blurToSave: true,
+};
+
+const statusFormatter = (cell) => {
+    let styleClass;
+    if (cell === '고장') {
+        styleClass = 'bg-danger';
+    } else if (cell === '점검중') {
+        styleClass = 'bg-warning';
+    } else {
+        styleClass = 'bg-success';
+    }
+
+    return (
+        <span className={`p-2 rounded-circle d-inline-block ${styleClass}`}></span>
+    );
+};
+
+const Datatables = () => {
+    const navigate = useNavigate(); // useNavigate 추가
+    const dispatch = useDispatch();
+    const equipmentList = useSelector((state) => state.equipments.equipmentList);
+    console.log('equipmentList',equipmentList)
+
+    useEffect(() => {
+        dispatch(fetchEquipments());
+    }, [dispatch]);
+
+    if (!equipmentList || !equipmentList.data) {
+        return <div>Loading...</div>;
+    }
+
+    const options = {
+        afterDeleteRow: onAfterDeleteRow,
+        afterSearch,
+        onRowClick: (row) => {
+            navigate(`/equipment/${row.equipmentCode}`); // 페이지 네비게이션
+        },
+    };
+
+    return (
+        <div>
+            <Card>
+                <CardBody>
+                    <CardTitle tag="h5">장비 재고</CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    장비 재고 전체 조회
+                    </CardSubtitle>
+                    <BootstrapTable
+                        hover
+                        search
+                        data={equipmentList.data}
+                        keyField='equipmentCode'
+                        insertRow
+                        deleteRow
+                        selectRow={selectRowProp}
+                        pagination
+                        options={options}
+                        cellEdit={cellEditProp}
+                        tableHeaderClass="mb-10"
+                        exportCSV
+                        headerStyle={{ width: '100%' }}
+                    >
+                        <TableHeaderColumn
+                            width="20%"
+                            dataField="equipmentName"
+                            dataAlign="center"
+                            // isKey
+                            dataFormat={(cell, row) => (
+                                <div onClick={() => navigate(`/equipment/${row.equipmentCode}`)}>
+                                    {cell}
+                                </div>
+                            )}
+                        >
+                            Name
+                        </TableHeaderColumn>
+                        <TableHeaderColumn
+                            width="20%"
+                            dataField="equipmentLocation"
+                            dataAlign="center"
+                            dataFormat={(cell, row) => (
+                                <div onClick={() => navigate(`/equipment/${row.equipmentCode}`)}>
+                                    {cell}
+                                </div>
+                            )}
+                        >
+                            Location
+                        </TableHeaderColumn>
+                        <TableHeaderColumn
+                            width="20%"
+                            dataField="status"
+                            dataAlign="center"
+                            dataFormat={(cell, row) => (
+                                <div onClick={() => navigate(`/equipment/${row.equipmentCode}`)}>
+                                    {statusFormatter(cell, row)}
+                                </div>
+                            )}
+                        >
+                            Status
+                        </TableHeaderColumn>
+
+                        <TableHeaderColumn
+                            width="20%"
+                            dataField="equipmentQuantity"
+                            dataAlign="center"
+                            dataFormat={(cell, row) => (
+                                <div onClick={() => navigate(`/equipment/${row.equipmentCode}`)}>
+                                    {cell}
+                                </div>
+                            )}
+                        >
+                            EquipmentQuantity
+                        </TableHeaderColumn>
+                        <TableHeaderColumn
+                            width="20%"
+                            dataField="equipmentManager"
+                            dataAlign="center"
+                            dataFormat={(cell, row) => (
+                                <div onClick={() => navigate(`/equipment/${row.equipmentCode}`)}>
+                                    {cell}
+                                </div>
+                            )}
+                        >
+                            Manager
+                        </TableHeaderColumn>
+                    </BootstrapTable>
+                </CardBody>
+            </Card>
+        </div>
+    );
+};
+
+export default Datatables;
---
+++ src/views/equipment/equipmentDetail.js
@@ -0,0 +1,214 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Button,
+  FormFeedback,
+} from 'reactstrap';
+import BreadCrumbs from '../../layouts/breadcrumbs/BreadCrumbs';
+import { useParams, useNavigate } from 'react-router-dom'; 
+import { useDispatch, useSelector } from 'react-redux'; 
+import { fetchEquipment, modifyEquipment, deleteEquipment } from '../../store/apps/equipment/equipmentSlice'; 
+
+const EquipmentDetail = () => {
+  const { equipmentCode } = useParams();
+  const [readOnly, setReadOnly] = useState(true);
+  const [equipmentInfo, setEquipmentInfo] = useState({});
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const equipmentDetail = useSelector(state => state.equipments.equipmentDetail);
+  
+
+
+  useEffect(() => {
+    dispatch(fetchEquipment({ equipmentCode }));
+    
+
+  }, [dispatch, equipmentCode]);
+
+
+  useEffect(() => {
+    if (equipmentDetail && equipmentDetail.data) {
+      setEquipmentInfo(equipmentDetail.data);
+    }
+  }, [equipmentDetail]);
+
+
+
+  const onChangeHandler = e => {
+    const { name, value } = e.target;
+    setEquipmentInfo({ 
+      ...equipmentInfo,
+      [name]: value } );
+  };
+
+  const handleSave = () => {
+    dispatch(modifyEquipment({ equipmentCode, equipmentInfo: equipmentInfo }))
+    navigate('/equipment');
+
+  
+  };
+
+  const handleDelete = () => {
+    dispatch(deleteEquipment({ equipmentCode}))
+    navigate('/equipment');
+  
+  };
+
+
+
+  return (
+    <div>
+      <BreadCrumbs />
+      <Row>
+        <Col md="12">
+          <Card>
+            <CardBody className="bg-light">
+              <CardTitle tag="h2" className="mb-0">Inspection</CardTitle>
+            </CardBody>
+            <CardBody>
+              <Form>
+                <Row>
+                  <Col md="6">
+                    <FormGroup>
+                      <Label>Location</Label>
+                      <Input
+                        type="text"
+                        placeholder="안전점검 할 위치를 입력하세요"
+                        name="location"
+                        onChange={onChangeHandler}
+                        readOnly={readOnly}
+                        value={equipmentInfo.location || ''}
+                      />
+                      <FormFeedback valid>Success! You've done it.</FormFeedback>
+                    </FormGroup>
+                  </Col>
+                  <Col md="6">
+                    <FormGroup>
+                      <Label>Status</Label>
+                      <Input
+                        type="select"
+                        name="status"
+                        onChange={onChangeHandler}
+                        disabled={readOnly}
+                        value={equipmentInfo.status || ''}
+                      >
+                        <option value="정상">정상</option>
+                        <option value="점검중">점검중</option>
+                        <option value="중단">중단</option>
+                      </Input>
+                      <FormText className="muted">This field has error.</FormText>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md="6">
+                    <FormGroup>
+                      <Label>Type</Label>
+                      <Input
+                        type="select"
+                        name="type"
+                        onChange={onChangeHandler}
+                        disabled={readOnly}
+                        value={equipmentInfo.type || ''}
+                      >
+                        <option value="점포">점포</option>
+                        <option value="안내소">안내소</option>
+                      </Input>
+                      <FormText className="muted">Select your type</FormText>
+                    </FormGroup>
+                  </Col>
+                  <Col md="6">
+                    <FormGroup>
+                      <Label>Manager</Label>
+                      <Input
+                        type="text"
+                        name="manager"
+                        placeholder="이름을 입력하세요"
+                        readOnly={readOnly}
+                        onChange={onChangeHandler}
+                        value={equipmentInfo.manager || ''}
+                      />
+                      <FormText className="muted">이름은 반드시 입력해야 합니다.</FormText>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md="6">
+                    <FormGroup>
+                      <Label>Regular Inspection Date</Label>
+                      <Input
+                        type="text"
+                        name="regularInspectionDate"
+                        placeholder="점검일을 기입하세요"
+                        readOnly={readOnly}
+                        onChange={onChangeHandler}
+                        value={equipmentInfo.regularInspectionDate || ''}
+                      />
+                    </FormGroup>
+                  </Col>
+                  <Col md="6">
+                    <FormGroup>
+                      <Label>Phone</Label>
+                      <Input
+                        type="text"
+                        name="phone"
+                        placeholder="010-****-****"
+                        readOnly={readOnly}
+                        onChange={onChangeHandler}
+                        value={inspectionInfo.phone || ''}
+                      />
+                      <FormText className="muted">휴대폰 번호는 반드시 입력해야 합니다.</FormText>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md="6">
+                    <FormGroup>
+                      <Label>비고</Label>
+                      <Input
+                        type="textarea"
+                        placeholder="특이사항을 입력하세요"
+                        rows="6"
+                        name="text" // 기존 "text"에서 변경
+                        readOnly={readOnly}
+                        onChange={onChangeHandler}
+                        value={inspectionInfo.text || ''} // 기존 "text"에서 변경
+                      />
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Col className="d-flex justify-content-center">
+                  {readOnly ? (
+                    <Button className="btn" color="primary" onClick={() => setReadOnly(false)}>
+                      수정
+                    </Button>
+                  ) : (
+                    <>
+                      <Button className="btn" color="success" onClick={handleSave}>
+                        저장
+                      </Button>
+                      <Button className="btn" color="secondary" onClick={handleDelete}>
+                        삭제
+                      </Button>
+                    </>
+                  )}
+                </Col>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default EquipmentDetail;
